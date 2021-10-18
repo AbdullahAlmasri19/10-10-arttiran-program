636d994dc5f98172eddcc33f8851d2ba63bbee21
AbdullahAlmasri19 <92318757+AbdullahAlmasri19@users.noreply.github.com>
2021-10-18 20:32:28 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10_arttiran_program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            for (int i = 10; i <= 110; i+=10)
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            for (int i = 110; i >= 10; i-=10)
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+    }
+}
